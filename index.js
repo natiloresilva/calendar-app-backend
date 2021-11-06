6d67f0f3c0989b4114d1828dcc8fa60b94a4dc2c
Natalia <natalia.pupushi@gmail.com>
2021-11-06 12:57:37 +0100

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { dbConnection } = require('./database/config')
+var cors = require('cors')
 require('dotenv').config()
 
 
@@ -8,6 +9,10 @@ const app = express()
 
 //Base de datos
 dbConnection()
+
+// CORS
+app.use(cors())
+
 
 //Directorio Público. EL USE en express, es conocido como un middleware, que no es más que una función que se ejecuta en el momento que alguien hace una petición a mi servidor
 app.use(express.static('public'))
